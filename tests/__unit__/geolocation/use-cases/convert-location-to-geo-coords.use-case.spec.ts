7ea569763bd425d13c04921ab2cdc8e1234a696a
Matheus Torres <matheustrres.rj@gmail.com>
2024-12-26 19:45:48 -0300

    test: Ensure SUT returns the locations found for given address

--- tests/__unit__/geolocation/use-cases/convert-location-to-geo-coords.use-case.spec.ts
+++ tests/__unit__/geolocation/use-cases/convert-location-to-geo-coords.use-case.spec.ts
@@ -48,4 +48,45 @@ describe(ConvertLocationToGeoCoordinatesUseCase.name, () => {
 			stateCode: undefined,
 		});
 	});
+
+	it('should return the locations found with given address', async () => {
+		jest
+			.spyOn(geocodingService, 'convertAddressToGeoCoordinates')
+			.mockResolvedValueOnce({
+				status: GeocodingRequestStatus.Success,
+				data: [
+					{
+						name: 'São Paulo',
+						lat: -23.5506507,
+						lon: -46.6333824,
+						country: 'BR',
+						state: 'São Paulo',
+					},
+				],
+			});
+
+		const result = await sut.exec({
+			city: 'São Paulo',
+			countryCode: 'BR',
+			limit: 5,
+		});
+
+		expect(
+			geocodingService.convertAddressToGeoCoordinates,
+		).toHaveBeenCalledWith({
+			city: 'São Paulo',
+			countryCode: 'BR',
+			limit: 5,
+			stateCode: undefined,
+		});
+		expect([...result.locations]).toStrictEqual([
+			{
+				name: 'São Paulo',
+				lat: -23.5506507,
+				lon: -46.6333824,
+				country: 'BR',
+				state: 'São Paulo',
+			},
+		]);
+	});
 });
