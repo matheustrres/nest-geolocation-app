ad3b5bb364e7cd56911fe1b0a6b07722f3e29ef3
Matheus Torres <matheustrres.rj@gmail.com>
2024-12-27 22:04:36 -0300

    test: ensure SUT returns the locations found for given coordinates

--- tests/__unit__/geolocation/use-cases/convert-geo-coords-to-location.use-case.spec.ts
+++ tests/__unit__/geolocation/use-cases/convert-geo-coords-to-location.use-case.spec.ts
@@ -48,4 +48,71 @@ describe(ConvertGeoCoordsToLocationUseCase.name, () => {
 			skip: undefined,
 		});
 	});
+
+	it('should return the locations found for given coordinates', async () => {
+		jest
+			.spyOn(geocodingService, 'convertGeoCoordinatesToAddress')
+			.mockResolvedValueOnce({
+				status: GeocodingRequestStatus.Success,
+				data: [
+					{
+						city: 'São Paulo',
+						country: 'Brasil',
+						state: 'São Paulo',
+						lat: -23.5506507,
+						lon: -46.6333824,
+						street: 'Avenida Paulista',
+					},
+					{
+						city: 'São Paulo',
+						country: 'Brasil',
+						state: 'São Paulo',
+						lat: -23.5506507,
+						lon: -46.6333824,
+						street: 'Avenida Paulista',
+					},
+					{
+						city: 'São Paulo',
+						country: 'Brasil',
+						state: 'São Paulo',
+						lat: -23.5506507,
+						lon: -46.6333824,
+						street: 'Avenida Paulista',
+					},
+				],
+			});
+
+		const { input } = new ConvertGeoCoordsToLocationUseCaseBuilder()
+			.setLat(-23.5505)
+			.setLon(-46.6333)
+			.setLimit(2);
+
+		const result = await sut.exec(input);
+
+		expect(
+			geocodingService.convertGeoCoordinatesToAddress,
+		).toHaveBeenCalledWith({
+			lat: -23.5505,
+			lon: -46.6333,
+		});
+		expect(result.locations).toHaveLength(2);
+		expect(result.locations).toStrictEqual([
+			{
+				city: 'São Paulo',
+				country: 'Brasil',
+				state: 'São Paulo',
+				lat: -23.5506507,
+				lon: -46.6333824,
+				street: 'Avenida Paulista',
+			},
+			{
+				city: 'São Paulo',
+				country: 'Brasil',
+				state: 'São Paulo',
+				lat: -23.5506507,
+				lon: -46.6333824,
+				street: 'Avenida Paulista',
+			},
+		]);
+	});
 });
